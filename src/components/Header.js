e25f035a5cb51301f2fff013c51962915b7bb774
Hima Sandeep Vartha <Sandeep.Vartha@dhanvita.com>
2025-05-08 18:13:17 +0530

    adjusted netflix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
   return (
     <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
       <img
-        className="w-44"
+         className="w-32 h-20"
         src={LOGO}
         alt="Netflix_Logo"
       />
