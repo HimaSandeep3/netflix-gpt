389458e129460c91e0e2731f029d7bbba522a547
Hima Sandeep Vartha <Sandeep.Vartha@dhanvita.com>
2025-05-13 00:52:03 +0530

    after selecting automatically scrolled to top

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from './Header'
 import useNowPlayingMovies from '../hooks/useNowPlayingMovies';
 import usePopularMovies from '../hooks/usePopularMovies';
@@ -10,18 +10,35 @@ import GPTSearch from './GPTSearch';
 import { useSelector } from 'react-redux';
 
 const Browse = () => {
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [playWithAudio, setPlayWithAudio] = useState(false);
   useNowPlayingMovies();
   usePopularMovies();
   useTopRatedMovies();
   useUpComingMovies();
   const showGPTSearch = useSelector((store) => store.gpt.showGPTSearch);
+  const handleMovieClick = (movie) => {
+    setSelectedMovie(movie);
+    setPlayWithAudio(false); // Reset audio if needed
+
+    // 🔽 Scroll to top
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
   return (
     <div>
       <Header />
       {showGPTSearch ? <GPTSearch />:
       <>
-      <MainContainer />
-      <SecondaryContainer />
+      <MainContainer
+            selectedMovie={selectedMovie}
+            playWithAudio={playWithAudio}
+            onPlayClick={() => setPlayWithAudio(true)}
+          />
+          <SecondaryContainer onMovieClick={handleMovieClick} // Reset mute for new video until user clicks play
+        />
       </>
     }
     </div>
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { IMG_PATH } from '../utils/constants'
 
-const MovieCard = ({posterPath}) => {
+const MovieCard = ({posterPath,onClick}) => {
   if (!posterPath) {
     return null;
   }
   return (
-    <div className="min-w-[150px] md:min-w-[250px] hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer">
+    <div className="min-w-[150px] md:min-w-[250px] hover:scale-105 transition-transform duration-300 ease-in-out cursor-pointer" onClick={onClick}>
       <img
         className="rounded-lg shadow-lg h-80 w-full object-cover"
         src={IMG_PATH + posterPath}
